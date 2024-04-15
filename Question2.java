c0e9bf3fa5d450bcf6bea19ba590f493a5152b77
Klaris1234 <148183626+Klaris1234@users.noreply.github.com>
2024-04-15 14:51:33 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -12,7 +12,7 @@ public class Question2
      
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
-    double weight= in.nextDOuble();
+    double weight= in.nextDouble();
     double BMI=weight/(height*height);
     System.out.println(BMI);
 

fa29a07717a45a389d13c9c6352a3b9347be3603
Klaris1234 <148183626+Klaris1234@users.noreply.github.com>
2024-04-15 14:55:10 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -11,6 +11,9 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    double enteredint = in.nextDouble();
+    double result= enteredint*enteredint;
+    System.out.println(result);
+
   }
 }
